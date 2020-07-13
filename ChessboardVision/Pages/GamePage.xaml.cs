a62053dbad0089ddbd0ef6270f29ddc8be199d82
Andrea <35015674+Sgridev@users.noreply.github.com>
2020-07-13 16:11:40 +0200

    first commit

---
+++ ChessboardVision/Pages/GamePage.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ChessboardVision
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GamePage : ContentPage
+    {
+        private readonly bool timePressure;
+        private int gameResult;
+        private Task progressBarFinish;
+        private int currentScore = 0;
+        private int currentLifes = 3;
+        public GamePage()
+        {
+            InitializeComponent();
+        }
+
+        public GamePage(bool timePressure)
+        {
+            InitializeComponent();
+            this.timePressure = timePressure;
+            
+
+        }
+
+        protected async override void OnAppearing()
+        {
+
+            if (timePressure)
+            {
+                progressBar.IsVisible = true;
+                progressBarFinish = WaitProgressBarAsync();
+
+            }
+
+            square.Text = Chessboard.GetRandomSquare();
+
+            if (timePressure)
+            {
+                await progressBarFinish;
+                await Navigation.PushModalAsync(new ScorePage(currentScore, timePressure));
+            }
+
+        }
+
+        private async Task WaitProgressBarAsync()
+        {
+            await progressBar.ProgressTo(1, 60000, Easing.Linear);
+
+        }
+
+        private void IsLight_OnClicked(object sender, EventArgs e)
+        {
+            CheckColorInput(true);
+          
+
+        }
+        private void IsDark_OnClicked(object sender, EventArgs e)
+        {
+            CheckColorInput(false);
+
+        }
+
+        private void CheckColorInput(bool isLight)
+        {
+            if (Chessboard.IsColorCorrect(square.Text, isLight))
+            {
+                ++currentScore;
+                score.Text = currentScore.ToString();
+
+            }
+            else
+                RemoveLife();
+
+            square.Text = Chessboard.GetRandomSquare();
+        }
+
+        private async void RemoveLife()
+        {
+            currentLifes--;
+            if (currentLifes == 2)
+                life3.IsVisible = false;
+            else if(currentLifes == 1)
+                life2.IsVisible = false;
+            else
+            {
+
+                life1.IsVisible = false;
+                await Navigation.PushModalAsync(new ScorePage(currentScore, timePressure));
+            }
+
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            return true;
+        }
+    }
+}
---
+++ ChessboardVision/Pages/ScorePage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ChessboardVision
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ScorePage : ContentPage
+    {
+        private int gameScore;
+        private bool timePressure;
+
+
+
+        public ScorePage(int currentScore1, bool timePressure)
+        {
+            InitializeComponent();
+            this.gameScore = currentScore1;
+            this.timePressure = timePressure;
+            
+            currentScore.Text = gameScore.ToString();
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            return true;
+        }
+    }
+}
